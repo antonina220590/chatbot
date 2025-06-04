0660b5fa28e8ff9632c2c3c3c269f3eeb86bf5af
antonina220590 <tonya2290@gmail.com>
2025-06-04 09:50:08 +0300

    chore: set up eslint and prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,6 +1,7 @@
-import { dirname } from "path";
-import { fileURLToPath } from "url";
-import { FlatCompat } from "@eslint/eslintrc";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import { FlatCompat } from '@eslint/eslintrc';
+import eslintConfigPrettier from 'eslint-config-prettier';
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -10,7 +11,41 @@ const compat = new FlatCompat({
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+  ...compat.extends('next/core-web-vitals', 'next/typescript'),
+
+  {
+    files: ['**/*.{ts,tsx}'],
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'error',
+      'no-console': 'error',
+      '@typescript-eslint/no-unused-vars': [
+        'error',
+        {
+          varsIgnorePattern: '^_',
+          argsIgnorePattern: '^_',
+          ignoreRestSiblings: true,
+        },
+      ],
+      'no-inline-comments': 'error',
+      'no-restricted-syntax': [
+        'error',
+        {
+          selector: 'BlockComment',
+          message: 'Use JSDoc/TSDoc for documentation.',
+        },
+        {
+          selector:
+            'LineComment:not(Program > LineComment, :matches([id.name=\"TODO\"],[id.name=\"FIXME\"]))',
+          message:
+            "Comments are discouraged. Explain 'why', not 'what'. Use JSDoc/TSDoc for APIs.",
+        },
+      ],
+    },
+  },
+  {
+    ignores: ['dist', 'node_modules', '{.,}next/'],
+  },
+  eslintConfigPrettier,
 ];
 
 export default eslintConfig;
