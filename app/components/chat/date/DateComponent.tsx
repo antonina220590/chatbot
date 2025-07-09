05e346828dc2292ef0d783db40e514f0560a9a14
antonina220590 <tonya2290@gmail.com>
2025-07-09 19:45:24 +0300

    fix: add currentDate into DateComponent

--- app/components/chat/date/DateComponent.tsx
+++ app/components/chat/date/DateComponent.tsx
@@ -13,7 +13,7 @@ export default function DateComponent() {
   return (
     <div className="flex justify-center pb-[17px]">
       <p className="font-body font-normal text-[10px] sm:text-[12px] text-text-gray">
-        {dayjs().format('MM/DD/YYYY')}
+        {currentDate}
       </p>
     </div>
   );
